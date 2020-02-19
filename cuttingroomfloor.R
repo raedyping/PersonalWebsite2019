65fadcc61087483cc5f005e0c02431e3ba771182
Raedy Ping <raedyping@gmail.com>
2020-02-18 19:34:14 -0600

    updating website with new cv

---
+++ cuttingroomfloor.R
@@ -0,0 +1,6 @@
+  * Collaborating with psychologists, neuroscientists, teachers, and geoscientists on a longitudinal quasi-experiment seeking neural markers of conceptual understanding
+  
+    * Big picture strategizing that makes sense in practice and in theory and communicating those strategies to people at various levels of the hierarchy
+  
+  
+  I think this paper--the example I attached--will get accepted to this person's conference, but I wanted to do a deeper dive into the data and explore secondary research questions. For survey data, it might include doing psychometrics on item-level data to help you figure out what your surveys are really measuring, structural and latent analyses (eg Structural Equation Modeling) to examine issues of causality, visualizing the data lots of ways. This kind of exploration and testing usually results in a more interesting paper that makes a more unique contribution to the literature. 
